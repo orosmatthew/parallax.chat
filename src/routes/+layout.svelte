490b0d5f9de87b5e681a67afccf98c51ba958f66
orosmatthew <orosmatthew@pm.me>
2023-05-12 18:16:14 -0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	import { onMount } from 'svelte';
+	onMount(async () => {
+		await import('bootstrap');
+	});
+
+	type theme = 'light' | 'dark';
+
+	let currentTheme: theme = 'light';
+
+	function onThemeSwitch(event: Event) {
+		currentTheme = (event.target as HTMLInputElement).checked ? 'dark' : 'light';
+		document.getElementById('html')?.setAttribute('data-bs-theme', currentTheme);
+	}
+</script>
+
+<body class="container">
+	<div class="form-check form-switch">
+		<label for="theme_switch">Theme Switch</label>
+		<input
+			id="theme_switch"
+			on:change={onThemeSwitch}
+			class="form-check-input"
+			type="checkbox"
+			role="switch"
+			checked
+		/>
+	</div>
+	<slot />
+</body>
